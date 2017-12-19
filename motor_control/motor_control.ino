f6b478fad5602d45c9ed5b04e811c8e13fbb4261
josuerocha <josuerocha@me.com>
2017-12-19 13:52:35 -0200

    [MAJOR] Motor control module and pictures

---
+++ motor_control/motor_control.ino
@@ -0,0 +1,70 @@
+int RPWM_MOTOR1 = 3; //CLOCKWISE SPIN
+int LPWM_MOTOR1 = 4; //COUNTERCLOCKWISE SPIN
+
+
+int RPWM_MOTOR2 = 5; //CLOCKWISE SPIN
+int LPWM_MOTOR2 = 6; //COUNTERCLOCKWISE SPIN
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RPWM_MOTOR1,OUTPUT);
+  pinMode(LPWM_MOTOR1,OUTPUT);
+  pinMode(RPWM_MOTOR2,OUTPUT);
+  pinMode(LPWM_MOTOR2,OUTPUT);
+  
+}
+
+void loop() {
+
+  delay(500);
+  analogWrite(RPWM_MOTOR1,0);
+  analogWrite(LPWM_MOTOR1,0);
+  analogWrite(RPWM_MOTOR2,0);
+  analogWrite(LPWM_MOTOR2,0);
+  
+  
+  for(int i=100; i< 256 ; i++){
+    analogWrite(RPWM_MOTOR1,i);
+    analogWrite(RPWM_MOTOR2,i);
+    delay(50);
+  }
+
+  delay(500);
+
+  analogWrite(RPWM_MOTOR1,0);
+  analogWrite(LPWM_MOTOR1,0);
+  analogWrite(RPWM_MOTOR2,0);
+  analogWrite(LPWM_MOTOR2,0);
+  
+  for(int i=100; i< 256 ; i++){
+    analogWrite(LPWM_MOTOR1,i);
+    analogWrite(LPWM_MOTOR2,i);
+    delay(50);
+  }
+
+  delay(500);
+
+  analogWrite(RPWM_MOTOR1,0);
+  analogWrite(LPWM_MOTOR1,0);
+  analogWrite(RPWM_MOTOR2,0);
+  analogWrite(LPWM_MOTOR2,0);
+  
+  for(int i=100; i< 256 ; i++){
+    analogWrite(LPWM_MOTOR1,i);
+    analogWrite(RPWM_MOTOR2,i);
+    delay(50);
+  }
+
+  delay(500);
+
+  analogWrite(RPWM_MOTOR1,0);
+  analogWrite(LPWM_MOTOR1,0);
+  analogWrite(RPWM_MOTOR2,0);
+  analogWrite(LPWM_MOTOR2,0);
+
+  for(int i=100; i< 256 ; i++){
+    analogWrite(RPWM_MOTOR1,i);
+    analogWrite(LPWM_MOTOR2,i);
+    delay(50);
+  }
+}
